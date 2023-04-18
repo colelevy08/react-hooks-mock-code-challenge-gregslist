94ffe52c16430d811aa35cdee1f5294778d2f3f2
colelevy <colelevy08@gmail.com>
2023-04-18 13:01:23 -0400

    CodeChallengePractice

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,17 @@
 import React from "react";
 import Search from "./Search";
 
-function Header() {
+function Header({ setSearchTerm }) {
   return (
     <header>
-      <h1>
-        <span className="logo" role="img">
-          ☮
-        </span>
-        gregslist
-      </h1>
-      <Search />
+      <h1>FreeBay</h1>
+      <Search setSearchTerm={setSearchTerm} />
     </header>
   );
 }
 
 export default Header;
+
+
+
+
